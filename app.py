799c9917e6a2e8343775a199712b5cc9a606e0bd
alexxozo <simion.alexgabriel@yahoo.com>
2021-07-27 21:31:12 +0200

    added form fields

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.title("Test")
+with st.form("form"):
+	# age (18, 64)
+	age = st.slider('Age', 18, 64)
+	# sex (m, f)
+	sex = st.selectbox('Sex',('m', 'f'))
+	# bmi (16, 53.1)
+	bmi = st.slider('BMI', 16, 54)
+	# children (0, 5)
+	children = st.slider('Children number', 0, 5)
+	# smoker (y, n)
+	smoker = st.selectbox('Are you a smoker?',('yea', 'nah'))
+	# region (southeast, southwest, other)
+	region = st.selectbox('Select a region',('southeast', 'southwest', 'other'))
+	# charges
+
+	submitted = st.form_submit_button("Submit")
+
