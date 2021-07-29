66ec31837cc9f7aeb261355dc02da8562917516a
Alex Mtz <alejandro@oneragtime.com>
2021-07-29 20:05:03 +0200

    better prompts

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ st.markdown(
 
 with st.form("form"):
     # age (18, 64)
-    age = st.slider("Age", 18, 64)
+    age = st.slider("How old are you?", 18, 64)
     # sex (m, f)
-    sex = st.selectbox("Sex", ("m", "f"))
+    sex = st.selectbox("What is your Sex?", ("m", "f"))
     # bmi (16, 53.1)
 
-    height = st.slider("height (cm)", 100, 210)
-    weight = st.slider("weight (Kg)", 20, 160)
+    height = st.slider("What is your height? (cm)", 100, 210)
+    weight = st.slider("How much do you weight? (Kg)", 20, 160)
 
     # bmi = st.slider("BMI", 16, 54)
     # children (0, 5)
-    children = st.slider("Children number", 0, 5)
+    children = st.slider("How many children do you have?", 0, 5)
     # smoker (y, n)
     smoker = st.selectbox("Are you a smoker?", ("yes", "no"))
     # region (southeast, southwest, other)
@@ -29,7 +29,7 @@ with st.form("form"):
     )
     # charges
 
-    submitted = st.form_submit_button("Submit")
+    submitted = st.form_submit_button("Calculate price")
 
     if submitted:
         bmi = weight / (height / 100) ** 2
